d9bd9fd9151e79d4a16c4a9153dd6c78fabe33bf
Adam Szakal <adam.b.szakal@gmail.com>
2018-12-20 15:05:03 +0100

    stuff

--- parallax/sketch.js
+++ parallax/sketch.js
@@ -7,7 +7,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight * .6 , WEBGL).parent('canvas-container');
+  createCanvas(windowWidth, windowHeight * .8 , WEBGL).parent('canvas-container');
   frameRate(30);
   stroke(230);
   angleMode(DEGREES);
@@ -21,19 +21,20 @@ function draw() {
 
   ambientLight(50);
   // directionalLight(c, windowWidth/2, windowHeight/2, -300);
-  orbitControl();
+  // orbitControl();
 
-  translate(rotationY * damp, rotationX * damp)
+  // translate(rotationY * damp, rotationX * damp + 20)
   // translate(-mouseX * damp, -mouseY * damp)
-  plane(300, 500).fill(230);
+  plane(windowWidth / 1.5, windowHeight/1.5);
+  fill(230);
 
-  translate(rotationY * damp * 2 - 100, rotationX * damp * 2, 50);
+  translate(rotationY * damp * 2 - 25, rotationX * damp * 2, 50);
   // translate(-mouseX * damp * 2 - 100, -mouseY * damp * 2);
   fill(240, 64, 20);
-  plane(250, 250);
+  plane(windowWidth / 1.65, windowHeight / 3);
 
-  translate(rotationY * damp * 3 - 100, rotationX * damp * 3, 50);
+  translate(rotationY * damp * 3 - 10, rotationX * damp * 3, 20);
   fill(230);
-  textSize(80);
+  textSize(32);
   text('Xофманн', -windowWidth/5, -windowHeight/16);
 }
