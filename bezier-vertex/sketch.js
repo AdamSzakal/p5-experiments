d872bc9aef2f97861ec81fe9d0030ddaff417ea1
Adam Szakal <adam.b.szakal@gmail.com>
2018-12-20 22:13:43 +0100

    bunch of stuff on bezier-vertex experiment

--- bezier-vertex/sketch.js
+++ bezier-vertex/sketch.js
@@ -3,61 +3,63 @@ var n = 12;
 var r = 200;
 var xpts = [];
 var ypts = [];
+var pts = [];
 var curvature = 0;
+var hval = 140;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(30);
   stroke(230);
   noFill();
+  colorMode(HSB, 255);
 
-  startVal = createSlider(0, 12, 1);
+  startVal = createSlider(0, 11, 1);
   startVal.position(10,10);
-  endVal = createSlider(0, 12, 1);
+  endVal = createSlider(0, 11, 3);
   endVal.position(10,50);
-}
 
-function draw() {
-  background(30);
-  translate(windowWidth/2, windowHeight/2);
-
-  strokeWeight(10);
-
-  // Create points and add to array
+  // Create array of points
   for (var i=0; i < n; i++) {
-    point(r * cos(angle), r * sin(angle));
-
-    xpts.push(r * cos(angle));
-    ypts.push(r * sin(angle));
+    
+    pts[i] = {
+      x: r * cos(angle),
+      y: r * sin(angle),
+      c: color(random(255), 100, 255)
+    }
 
     angle += TWO_PI / 12.0;
   }
-  
-  strokeWeight(1);
-
-
-  // TODO: Denna skiten nedan
-  if (startVal.value() - endVal.value() > 6 && startVal.value() ) {
-    curvature = 0;
-  } else if (startVal.value() - endVal.value() == 6) {
-    curvature = .5;
-  } else {
-    curvature = 1;
-  }
-
-  curveBetween(xpts[startVal.value()], ypts[startVal.value()], xpts[endVal.value()], ypts[endVal.value()], 0.3, 0.3 , curvature);
 }
 
+function draw() {
+  background(0, 0, 25);
+  translate(windowWidth/2, windowHeight/2);
 
-function curveBetween(x1, y1, x2, y2, d, h, flip) {
-  //find two control points off this line
-  var original = p5.Vector.sub(createVector(x2, y2), createVector(x1, y1));
-  var inline = original.copy().normalize().mult(original.mag() * d);
-  var rotated = inline.copy().rotate(radians(90)+flip*radians(180)).normalize().mult(original.mag() * h);
-  var p1 = p5.Vector.add(p5.Vector.add(inline, rotated), createVector(x1, y1));
-  //line(x1, y1, p1.x, p1.y); //show control line
-  rotated.mult(-1);
-  var p2 = p5.Vector.add(p5.Vector.add(inline, rotated).mult(-1), createVector(x2, y2));
-  //line(x2, y2, p2.x, p2.y); //show control line
-  bezier(x1, y1, p1.x, p1.y, p2.x, p2.y, x2, y2)
+  // Draw the dots
+  strokeWeight(40);
+  for (var i=0; i<n; i++) {
+    push();
+    stroke(pts[i].c);
+    point(pts[i].x, pts[i].y);
+    pop();
+  }
+
+  // Draw curbe between points by creating a bezier curve
+  push();
+  stroke(255,0,255,25);
+  
+  // Lines showing control points
+  strokeWeight(3);
+  line(pts[startVal.value()].x, pts[startVal.value()].y, pts[startVal.value()].x/2, pts[startVal.value()].y);
+
+  line( pts[[endVal.value()]].x/2, pts[[endVal.value()]].y/2, pts[[endVal.value()]].x, pts[[endVal.value()]].y);
+
+  // The curve
+  strokeWeight(40);
+  // TODO: measure distance between two points and use that as a amplitude for control points
+  bezier( pts[startVal.value()].x, pts[startVal.value()].y, pts[startVal.value()].x/1.75, pts[startVal.value()].y, pts[[endVal.value()]].x/1.75, pts[[endVal.value()]].y/1.75, pts[[endVal.value()]].x, pts[[endVal.value()]].y);
+
+  pop();
+
 }
